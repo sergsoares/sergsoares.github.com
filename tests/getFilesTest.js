255a999b0bded11057e1116fda5165304cb4f8ce
Sergio Soares <acad.sergio@gmail.com>
2018-08-19 18:28:12 -0300

    Refactored to assert inside tape tests.

--- tests/getFilesTest.js
+++ tests/getFilesTest.js
@@ -6,26 +6,26 @@ const test = _test(tape)
 import root from "root-path";
 const test_fixture_path = root() + "/tests/fixtures"
 
-test('should getFiles return Bluebird Promise', function (test) {
+test('should getFiles return Bluebird Promise', function (assert) {
     const value = getFiles(test_fixture_path, {})
-    test.true(BluebirdPromise.resolve(value) === value)
-    test.end()
+    assert.true(BluebirdPromise.resolve(value) === value)
+    assert.end()
 })
 
-test('should getFiles return Error when invalid path passed', function (test) {
+test('should getFiles return Error when invalid path passed', function (assert) {
     return getFiles("", {}).catch(function (value) {
-        test.true(value instanceof Error)
+        assert.true(value instanceof Error)
     })
 })
 
-test('should getFiles return Promise with array', function (test) {
+test('should getFiles return Promise with array', function (assert) {
     return getFiles(test_fixture_path, {}).then(function (value) {
-        test.true(Array.isArray(value))
+        assert.true(Array.isArray(value))
     })
 })
 
-test('should getFiles return Promise with array of Strings', function (test) {
+test('should getFiles return Promise with array of Strings', function (assert) {
     return getFiles(test_fixture_path, {}).then(function (value) {
-        test.false(value.find((item) => typeof item !== 'string' ))
+        assert.false(value.find((item) => typeof item !== 'string' ))
     })
 })
--- tests/mainTest.js
+++ tests/mainTest.js
@@ -1,7 +1,7 @@
 import test from 'tape'
 import main from "../src/main";
 
-test('Main JS finish with Success', function (test) {
-    test.equal(main.execute(), 'Build with success!')
-    test.end()
+test('Main JS finish with Success', function (assert) {
+    assert.equal(main.execute(), 'Build with success!')
+    assert.end()
 })
