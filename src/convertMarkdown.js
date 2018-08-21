5b76ed37b7f5a8536dd6a01db7a5527fec7e5b5a
Sergio Soares <acad.sergio@gmail.com>
2018-08-21 20:31:28 -0300

    Flow added to project to improve types in Javascript.

--- src/convertMarkdown.js
+++ src/convertMarkdown.js
@@ -1,7 +1,10 @@
+// @flow
 const markdown = require('markdown-it')('commonmark');
 
 //FIXME: Remove couple with post structure
-export default function (post) {
+export default function (post: any) {
+    console.log('asdfjlsadfj')
+
     post.body = markdown.render(post.body);
     return post;
 }
