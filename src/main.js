75daa90855d13cd7afd98b2b250200cc89cf7fa5
Sergio Soares <acad.sergio@gmail.com>
2018-08-21 00:14:46 -0300

    Added pug to project, created renderPost.js and flatten

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import getFiles from './getFiles'
 import YamlProcessor from "./YamlProcessor"
 import convertMarkdown from "./convertMarkdown"
 import writeHtml from "./writeHtml";
+import flattenPost from "./flattenPost";
 import root from 'root-path'
 const DEFAULT_POST = root() + '/posts'
 
@@ -9,7 +10,7 @@ async function execute() {
     return await getFiles(DEFAULT_POST, {})
         .map(YamlProcessor)
         .map(convertMarkdown)
-        // .map(value => { console.log(value) })
+        // .map(flattenPost)
         .map(writeHtml)
         .then(() => {
             return 'Build with success!'
